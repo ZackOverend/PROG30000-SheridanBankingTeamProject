e65de4e8dfbf3c31f158e5ba32150d6c636d3033
Julius Nillo <nilloj@sheridancollege.ca>
2024-11-04 10:50:20 -0500

    Added Accounts and Budgeting Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Accounts() 
+    {
+        return View();   
+    }
+
+    public IActionResult Budgeting()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- obj/Debug/net8.0/SheridanBankingTeamProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/SheridanBankingTeamProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SheridanBankingTeamProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1056158bb457278c1129543e99c9ce88188f0da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af9f43730d1162a683689af803a2e9d34959305")]
 [assembly: System.Reflection.AssemblyProductAttribute("SheridanBankingTeamProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SheridanBankingTeamProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
